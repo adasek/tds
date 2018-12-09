948fb9965a897e9bc1f27111c40859f9b807814e
Adam Benda <adam@adasek.cz>
2018-12-09 12:55:11 +0100

    Empty webpack project

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// See: https://www.valentinog.com/blog/webpack-tutorial/
+
+import World from './model/world';
+    console.log(`I'm a silly entry point`);
---
+++ src/model/world.js
@@ -0,0 +1,11 @@
+class World {
+
+    constructor(aspectRatio) {
+        this.height = 10000;
+        this.width = 10000 * aspectRatio;
+    }
+
+}
+
+
+export { World };
