0ee47a166cdfa0affda37e88150bf05850c18c1f
Adam Benda <adam@adasek.cz>
2018-12-17 09:38:38 +0100

    Webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
-    module.exports = {
-      module: {
+const path = require('path');
+
+module.exports = {
+    module: {
         rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             }
-          }
         ]
-      }
-    };
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public_html'),
+        compress: true,
+        port: 9111
+    }
+};
